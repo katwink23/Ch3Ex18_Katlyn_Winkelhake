ad7aa0261274cb6da366a8280460888fe699fa82
Katlyn W. <krw8ateez@gmail.com>
2024-09-23 14:11:59 -0400

    fixed

--- Ch3Ex18_Katlyn_Winkelhake.cpp
+++ Ch3Ex18_Katlyn_Winkelhake.cpp
@@ -4,7 +4,7 @@ File Name: Ch3Ex18_Katlyn_Winkelhake.cpp
 Date: 9/23/2024
 Programmer: Katlyn Winkelhake
 Requirements:
-Write a program that asks for the princple, interest rate, and the number of times interest is compounded.
+Write a program that asks for the principal, interest rate, and the number of times interest is compounded.
 It should display the report as shown in the example photo.
 */
 
@@ -16,30 +16,38 @@ using namespace std;
 
 int main()
 {
-	double principle;
-	double interestRate;
-	double interestTimes;
-	double interest;
-		interest = principle * pow(1 + (interestRate / interestTimes))(interestTimes);
-	double newSavings;
-		newSavings = principle + interest;
+	float principal;
+	float interestRate;
+	float totalInterest;
+	int interestTimes;
+	float interestYear;
+	float newSavings;
+		
 
+	cout << "What is your principal?" << endl;
+		cin >> principal;
 
-	cout << "What is your principle?" << endl;
-		cin >> principle;
-
-	cout << "What is your interest rate?" << endl;
+	cout << "What is your interest rate? (in percent)" << endl;
 		cin >> interestRate;
 
 	cout << "How many times is your interest compounded in a year?" << endl;
 		cin >> interestTimes;
 
+	interestRate /= 100;
+	interestYear = principal * pow((1 + interestRate / interestTimes), interestTimes);
+	totalInterest = interestYear - principal;
+	interestRate *= 100;
+
 	cout << setprecision(2) << showpoint << fixed;
-	cout << "Interest Rate:" << setw(6) << "$" << interestRate;
-	cout << "Times Compounded:" << setw(2) << interestTimes;
-	cout << "Principle:" << setw(10) << "$" << principle;
-	cout << "Interest:" << setw(11) << "$" << interest;
-	cout << "Amount in Savings:" << setw(1) << "$" << newSavings;
+	cout << endl;
+	cout << "Stats after a year:" << endl;
+	cout << " _________________ " << endl;
+	cout << endl;
+	cout << "Interest Rate:" << setw(9) << interestRate << "%" << endl;
+	cout << "Times Compounded:" << setw(3) << interestTimes << endl;
+	cout << "Principal:" << setw(10) << "$" << principal << endl;
+	cout << "Interest:" << setw(11) << "$" << totalInterest << endl;
+	cout << "Amount in Savings:" << setw(2) << "$" << interestYear << endl;
 
 
 	return 0;
